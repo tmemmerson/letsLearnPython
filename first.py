313a5cfac991e542c8e446d0d4815dd91c3389c4
Tristan Emmerson <tristan@stickerslug.com>
2021-01-07 12:32:11 -0800

    complete type exercise

--- first.py
+++ first.py
@@ -1,3 +1,20 @@
 import datetime
-print("Tristan is learning to code Python:")
-print("the date and time is", datetime.datetime.now())
+myNow = datetime.datetime.now()
+print(myNow)
+
+myNumber =3
+myText = "Tristan's favorite number is"
+
+x=10
+y="10"
+z=10.1
+sum1=x+x
+sum2=y+y
+sum3=z+z
+
+
+print(myText, myNumber)
+
+print(sum1, sum2, sum3)
+
+print(type(x), type(y), type(z))
