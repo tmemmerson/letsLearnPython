43601c3ffe10cd228ba65f1414b736ca0777df38
Tristan Emmerson <tristan@stickerslug.com>
2021-01-07 12:38:34 -0800

    create a range print and a stepped-range print

--- first.py
+++ first.py
@@ -18,3 +18,11 @@ print(myText, myNumber)
 print(sum1, sum2, sum3)
 
 print(type(x), type(y), type(z))
+
+list1=list(range(1,10))
+""" gives output of 1,2,3,4,5,6,7,8,9 """
+list2=list(range(1,10,2)) 
+""" gives output of 1,3,5,7,9 because the 3rd arguement is the step """ 
+student_grades=[1,2,3]
+
+print(list1,list2)
